158a31666abbefbd5bd7b8c809038a29b3b3aa31
Zui <zuikng@gmail.com>
2019-07-01 22:03:56 -0400

    Translate design into Vue
    * Add bootstrap-vue
    * Convert codepen design into Vue

---
+++ kard/src/components/Cards.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-card class="kard">
+    <b-card-body class="kard-body">
+      <div class="kard-text">
+        Question going to be here:
+        <h1>Blah blah</h1>
+      </div>
+      <div class="kard-answer">
+        <div class="input-group input-group-lg">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-lg"
+              >Answer</span
+            >
+          </div>
+          <input
+            type="text"
+            class="form-control"
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-lg"
+          />
+        </div>
+      </div>
+    </b-card-body>
+  </b-card>
+</template>
+
+<style lang="scss">
+.kard {
+  width: 40em;
+  height: 20em;
+  margin: 1em auto;
+  text-align: center;
+  .kard-body {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
---
+++ kard/src/views/Board.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>Kard</h1>
+    <b-row>
+      <b-col>
+        <Cards />
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Cards from '@/components/Cards.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    Cards,
+  },
+})
+export default class Board extends Vue {}
+</script>
